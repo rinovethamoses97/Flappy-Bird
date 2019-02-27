ea3b62ee14177608f630dc1fce85d018cb788ea3
rino <rinovethamoses97@gmail.com>
2019-02-27 22:22:10 +0530

    server listen function bug fixed

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(express.static(__dirname+"/public"));
 app.get("/",function(req,res){
     res.sendFile("index.html");
 });
-app.listen(process.env.PORT|3000,function(){
+app.listen(process.env.PORT||3000,function(){
     console.log("Server Running");
 });
